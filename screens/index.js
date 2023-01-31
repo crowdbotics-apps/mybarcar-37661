5bdf9a005eba83ad45eb3cd781a8144b46e231c4
Crowdbotics <team@crowdbotics.com>
2023-01-31 01:54:20 +0000

    Add

--- screens/index.js
+++ screens/index.js
@@ -1,203 +1,153 @@
 import React, { useState, useEffect } from "react";
-import { Text, StyleSheet, View, FlatList, Image } from "react-native";
+import { Text, View, StyleSheet, Image } from "react-native";
 
-const ProductListingScreen = (params) => {
-  const [products, setProducts] = useState([]);
+const OrderInvoice = () => {
+  const [order, setOrder] = useState({});
   useEffect(() => {
-    setProducts([
-      {
-        id: 1,
-        name: "Product name",
-        status: true,
-        isFavorite: false,
-        image: require("./assets/productImage.png")
-      },
-      {
-        id: 2,
-        name: "Product name",
-        status: false,
-        isFavorite: true,
-        image: require("./assets/productImage2.png")
-      },
-      {
-        id: 3,
-        name: "Product name",
-        status: true,
-        isFavorite: false,
-        image: require("./assets/productImage.png")
-      },
-      {
-        id: 4,
-        name: "Product name",
-        status: false,
-        isFavorite: true,
-        image: require("./assets/productImage2.png")
-      },
-      {
-        id: 5,
-        name: "Product name",
-        status: true,
-        isFavorite: false,
-        image: require("./assets/productImage.png")
-      },
-      {
-        id: 6,
-        name: "Product name",
-        status: false,
-        isFavorite: true,
-        image: require("./assets/productImage2.png")
-      }
-    ]);
+    setOrder({
+      name: "Product name",
+      totalPrice: 10,
+      image: require("./assets/productImage.png"),
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Auctor aliquam augue placerat mattis amet velit nisi nisl dui imperdiet et in. .",
+      promoPrice: 0,
+      deliveryFee: 0,
+      productPrice: 10,
+      status: "Delivered",
+      barcode: require("./assets/barcodeImage.png")
+    });
   }, []);
   return (
     <View style={styles.container}>
-      <TabView tabTitles={["All", "Best Products"]} selected={0} />
-      <View style={styles.productsContainer}>
-        <FlatList
-          data={products}
-          numColumns={2}
-          keyExtractor={(item) => item.id.toString()}
-          renderItem={({ item }) => <Product product={item} />}
-          columnWrapperStyle={{
-            justifyContent: "space-around"
-          }}
-          showsVerticalScrollIndicator={false}
-        />
+      <Text style={styles.heading}>Order Id</Text>
+      <Text style={styles.subHeading}>#2345123</Text>
+      <View style={styles.invoiceCard}>
+        <View style={styles.header}>
+          <Image source={order.image} style={styles.image} />
+          <Text style={styles.name}>{order.name}</Text>
+          <Text style={styles.price}>
+            ${order.totalPrice && order.totalPrice.toFixed(2)}
+          </Text>
+        </View>
+        <View style={styles.descriptionContainer}>
+          <Text style={styles.fnt16}>Order&apos;s descriptions</Text>
+          <Text style={styles.description}>{order.description}</Text>
+        </View>
+        <View style={styles.detailsContainer}>
+          <View style={styles.flexRow}>
+            <Text style={styles.fnt16}>Promo</Text>
+            <Text style={styles.fnt16}>
+              ${order.promoPrice && order.promoPrice.toFixed(2)}
+            </Text>
+          </View>
+          <View style={styles.flexRow}>
+            <Text style={styles.fnt16}>Delivery Fee</Text>
+            <Text style={styles.fnt16}>
+              ${order.deliveryFee && order.deliveryFee.toFixed(2)}
+            </Text>
+          </View>
+          <View style={styles.flexRow}>
+            <Text style={styles.fnt16}>Product Price</Text>
+            <Text style={styles.fnt16}>
+              ${order.productPrice && order.productPrice.toFixed(2)}
+            </Text>
+          </View>
+          <View style={styles.flexRow}>
+            <Text style={styles.fnt16}>Total Price</Text>
+            <Text style={styles.fnt16}>
+              ${order.totalPrice && order.totalPrice.toFixed(2)}
+            </Text>
+          </View>
+        </View>
+        <View style={styles.flexRow}>
+          <Text style={styles.fnt16}>Status</Text>
+          <Text>{order.status}</Text>
+        </View>
+        <Image source={order.barcode} style={styles.barcode} />
       </View>
     </View>
   );
 };
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff"
   },
-  productsContainer: {
-    paddingHorizontal: 20
-  }
-});
-export default ProductListingScreen;
-
-const TabView = ({ tabTitles, selected }) => {
-  return (
-    <View style={tabViewStyles.paletteContainer}>
-      {tabTitles.map((title, index) => (
-        <View
-          style={
-            index === selected
-              ? tabViewStyles.selected
-              : tabViewStyles.unSelected
-          }
-          key={index}
-        >
-          <Text>{title}</Text>
-        </View>
-      ))}
-    </View>
-  );
-};
-
-const tabViewStyles = StyleSheet.create({
-  paletteContainer: {
-    width: "70%",
-    height: 48,
-    backgroundColor: "#F1F1F1",
+  heading: {
+    fontSize: 24,
+    textTransform: "uppercase",
+    color: "#7C7C7C",
+    textAlign: "center"
+  },
+  subHeading: {
+    fontSize: 24,
+    textTransform: "uppercase",
+    color: "#000",
+    textAlign: "center"
+  },
+  invoiceCard: {
+    width: 340,
+    height: 570,
+    backgroundColor: "#F0F2F7",
+    alignSelf: "center",
+    marginTop: 20,
+    borderRadius: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 20
+  },
+  header: {
     flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
-    borderRadius: 10,
-    padding: 6,
-    marginVertical: 10,
-    marginHorizontal: 20
+    borderBottomWidth: 1,
+    paddingVertical: 20,
+    borderBottomColor: "#000"
   },
-  selected: {
-    borderRadius: 10,
-    flex: 1,
-    backgroundColor: "#fff",
-    height: "100%",
-    justifyContent: "center",
-    alignItems: "center",
-    shadowColor: "gray",
-    elevation: 10
+  image: {
+    width: 50,
+    height: 50,
+    borderRadius: 25
   },
-  unSelected: {
+  name: {
+    fontSize: 16,
+    color: "#3E4462",
     flex: 1,
-    height: "100%",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F1F1F1",
-    borderRadius: 10
-  }
-});
-
-const Product = ({ product }) => {
-  const availability = {
-    color: product.status ? "#12D790" : "#EA4335",
-    fontSize: 12,
+    marginLeft: 10
+  },
+  price: {
+    fontSize: 24,
     fontWeight: "bold"
-  };
-  return (
-    <View style={productStyles.container}>
-      <View style={productStyles.imageContainer}>
-        <Image source={product.image} style={productStyles.productImage} />
-
-        <Image
-          source={
-            product.isFavorite
-              ? require("./assets/isFavouriteIcon.png")
-              : require("./assets/favIcon.png")
-          }
-          style={productStyles.favIcon}
-        />
-      </View>
-      <View style={productStyles.descriptionContainer}>
-        <Text style={productStyles.bold}>{product.name}</Text>
-        <View style={productStyles.availabilityTextContainer}>
-          <Text style={productStyles.availabilityText}>Purchase: </Text>
-          <Text style={availability}>
-            {product.status ? "Available" : "Not available"}
-          </Text>
-        </View>
-      </View>
-    </View>
-  );
-};
-
-const productStyles = StyleSheet.create({
-  container: {
-    height: 240,
-    width: 160,
-    margin: 10
-  },
-  imageContainer: {
-    height: 180,
-    width: 160,
-    borderRadius: 10
-  },
-  productImage: {
-    height: "100%",
-    width: "100%",
-    borderRadius: 10
   },
   descriptionContainer: {
-    justifyContent: "center",
-    padding: 10
+    borderBottomColor: "#000",
+    borderBottomWidth: 1,
+    paddingVertical: 10
   },
-  availabilityTextContainer: {
+  description: {
+    marginVertical: 5,
+    textAlign: "justify"
+  },
+  fnt16: {
+    fontSize: 16
+  },
+  flexRow: {
     flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center"
   },
-  availabilityText: {
-    color: "#7C7C7C",
-    fontSize: 12,
-    fontWeight: "bold"
+  detailsContainer: {
+    paddingVertical: 20,
+    borderBottomColor: "#000",
+    borderBottomWidth: 1,
+    marginBottom: 10
   },
-  bold: {
-    fontWeight: "bold"
-  },
-  favIcon: {
-    position: "absolute",
-    right: 10,
-    top: 10
+  barcode: {
+    width: 250,
+    height: 100,
+    resizeMode: "contain",
+    alignSelf: "center",
+    marginTop: 40
   }
 });
+
+export default OrderInvoice;
