f238ee2990e5f253a592821d4bfaf0fd3c3e6edc
Crowdbotics <team@crowdbotics.com>
2023-01-31 01:53:59 +0000

    Add

--- screens/index.js
+++ screens/index.js
@@ -1,266 +1,72 @@
-import React, { useState } from "react";
+import React from "react";
 import {
   Text,
-  StyleSheet,
   View,
-  TextInput,
-  Image,
-  Pressable,
-  ScrollView
+  StyleSheet, Image, ScrollView, Pressable
 } from "react-native";
 
-const PaymentSubscriptionScreen = (params) => {
-  const [cardNumber, setCardNumber] = useState("");
-  const [cardExpiry, setCardExpiry] = useState("");
-  const [cvv, setCvv] = useState("");
-  const [name, setName] = useState("");
+const SubscriptionDetails = () => {
   return (
-    <View style={styles.container}>
-      <ScrollView>
-        <TabView tabTitles={["Beginner", "Intermediate", "VIP"]} selected={0} />
-        <View style={styles.subPallet}>
-          <View style={styles.planDes}>
-            <Text style={[styles.fnt25, styles.boldText]}>Beginner</Text>
-            <Text style={styles.fnt16}>Description</Text>
-          </View>
-          <View style={styles.subPricing}>
-            <Text style={[styles.fnt25, styles.boldText]}>$14.99 </Text>
-            <Text style={styles.fnt16}>per month</Text>
-          </View>
+    <ScrollView style={styles.container}>
+      <View style={styles.header}>
+        <Image source={require("./assets/back.png")} style={styles.back} />
+        <Text style={styles.heading}>Subscription</Text>
+        <Text />
+      </View>
+      <View style={styles.reviewContainer}>
+        <Text style={styles.review}>Current Subscription</Text>
+        <Text style={styles.edit}>Free</Text>
+      </View>
+
+      <View style={styles.listContainer}>
+        <Text style={styles.title}>Premium Subscription</Text>
+        <View style={styles.descContainer}>
+          <Text style={styles.desc}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas ultrices risus at neque, at. Cras cursus parturient nec, quam convallis viverra arcu. Lorem pellentesque cras vulputate velit ante tellus.
+          </Text>
+          <Text style={styles.desc}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec egestas ultrices risus at neque, at. Cras cursus parturient nec, quam convallis viverra arcu. Lorem pellentesque cras vulputate velit ante tellus.
+          </Text>
         </View>
-        <View style={styles.cardContainer}>
-          <View style={styles.tile}>
-            <Text style={styles.tileText}>Pay Subscription</Text>
-            <Image source={require("./assets/walletIcon.png")} />
-          </View>
-          <View style={styles.separator}></View>
-          <View style={styles.tile}>
-            <Text style={styles.tileText}>Add card</Text>
-            <Image source={require("./assets/addCardIcon.png")} />
-          </View>
+        <View style={styles.priceContainer}>
+          <Text style={styles.price}>Price:</Text>
+          <Text style={styles.price}> $14.99</Text>
         </View>
-        <View style={styles.inputs}>
-          <View style={styles.inputContainer}>
-            <Text style={styles.inputText}>Card Number</Text>
-            <TextInput
-              style={styles.input}
-              onChangeText={(text) => setCardNumber(text)}
-              value={cardNumber}
-              placeholder="Enter your Card Number"
-              placeholderTextColor="#9B9B9B"
-              autoCapitalize="none"
-              autoCorrect={false}
-            />
-          </View>
-          <View style={styles.halfInputs}>
-            <View style={styles.inputContainer}>
-              <Text style={styles.inputText}>Expiration Date</Text>
-              <TextInput
-                style={[styles.input, styles.input1]}
-                onChangeText={(text) => setCardExpiry(text)}
-                value={cardExpiry}
-                placeholder="Enter your last name"
-                placeholderTextColor="#9B9B9B"
-                autoCapitalize="none"
-                autoCorrect={false}
-              />
-            </View>
-            <View style={styles.inputContainer}>
-              <Text style={styles.inputText}>CVV</Text>
-              <TextInput
-                style={[styles.input, styles.input2]}
-                onChangeText={(text) => setCvv(text)}
-                value={cvv}
-                placeholder="CVV"
-                placeholderTextColor="#9B9B9B"
-                autoCapitalize="none"
-                autoCorrect={false}
-              />
-            </View>
-          </View>
-          <View style={styles.inputContainer}>
-            <Text style={styles.inputText}>Card Holder Name</Text>
-            <TextInput
-              style={styles.input}
-              onChangeText={(text) => setName(text)}
-              value={name}
-              placeholder="Username"
-              placeholderTextColor="#9B9B9B"
-              autoCapitalize="none"
-              autoCorrect={false}
-            />
-          </View>
-        </View>
-        <View style={styles.buttonsContainer}>
-          <Button buttonText={"Confirm"} />
-          <Button buttonText={"Cancel"} outline={true} />
-        </View>
-      </ScrollView>
-    </View>
+
+        <Button buttonText={"Upgrade to Premium"} />
+      </View>
+
+    </ScrollView>
   );
 };
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff"
+    flex: 1
   },
-  subPallet: {
-    marginHorizontal: 20,
-    backgroundColor: "#fff",
-    borderRadius: 10,
-    padding: 10,
+  header: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    shadowColor: "rgba(0,0,0,0.5)",
-    elevation: 10
+    marginHorizontal: 30,
+    marginTop: 15,
+    marginBottom: 30
   },
-  planDes: {
-    flex: 0.4,
-    padding: 10,
-    justifyContent: "center",
-    alignItems: "flex-start"
-  },
-  subPricing: {
-    flex: 0.6,
-    padding: 10,
-    flexDirection: "row",
-    alignItems: "flex-end",
-    justifyContent: "center"
-  },
-  boldText: {
-    fontWeight: "bold"
-  },
-  fnt25: {
-    fontSize: 25
-  },
-  fnt16: {
-    fontSize: 16
-  },
-  cardContainer: {
-    marginHorizontal: 20,
-    marginVertical: 20,
-    padding: 10,
-    elevation: 10,
-    backgroundColor: "#fff",
-    shadowColor: "rgba(0,0,0,0.5)",
-    borderRadius: 10
-  },
-  tile: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    padding: 10
-  },
-  separator: {
-    borderBottomColor: "#F0F2F7",
-    borderBottomWidth: 1
-  },
-  inputs: {
-    paddingHorizontal: 20,
-    justifyContent: "center",
-    height: 300
-  },
-  inputContainer: {
-    flexDirection: "column",
-    flex: 1,
-    justifyContent: "center"
-  },
-  inputText: {
-    fontSize: 14,
-    marginLeft: 20
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: "#e6e6e6",
-    borderRadius: 10,
-    padding: 10,
-    paddingLeft: 20,
-    marginVertical: 10,
-    width: "100%",
-    height: 50
-  },
-  halfInputs: {
-    justifyContent: "space-between",
-    flexDirection: "row"
-  },
-  input1: {
-    height: 50,
-    borderRightWidth: 0,
-    borderRightColor: "#fff",
-    borderTopRightRadius: 0,
-    borderBottomRightRadius: 0
-  },
-  input2: {
-    height: 50,
-    borderLeftWidth: 0,
-    borderLeftColor: "#fff",
-    borderTopLeftRadius: 0,
-    borderBottomLeftRadius: 0
-  },
-  buttonsContainer: {
-    flexDirection: "row",
-    justifyContent: "center",
-    paddingHorizontal: 20
-  }
-});
-export default PaymentSubscriptionScreen;
-
-const TabView = ({ tabTitles, selected }) => {
-  return (
-    <View style={tabViewStyles.paletteContainer}>
-      {tabTitles.map((title, index) => (
-        <View
-          style={
-            index === selected
-              ? tabViewStyles.selected
-              : tabViewStyles.unSelected
-          }
-          key={index}
-        >
-          <Text>{title}</Text>
-        </View>
-      ))}
-    </View>
-  );
-};
-
-const tabViewStyles = StyleSheet.create({
-  paletteContainer: {
-    marginHorizontal: 20,
-    width: "80%",
-    height: 48,
-    backgroundColor: "#F1F1F1",
-    flexDirection: "row",
-    alignItems: "center",
-    borderRadius: 10,
-    padding: 6,
-    marginVertical: 20
-  },
-  selected: {
-    borderRadius: 10,
-    flex: 1,
-    backgroundColor: "#fff",
-    height: "100%",
-    justifyContent: "center",
-    alignItems: "center",
-    shadowColor: "gray",
-    elevation: 10
-  },
-  unSelected: {
-    flex: 1,
-    height: "100%",
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F1F1F1",
-    borderRadius: 10
-  }
+  back: { width: 11.25, height: 20, resizeMode: "contain", marginLeft: -15 },
+  heading: { fontSize: 16, color: "#000" },
+  reviewContainer: { flexDirection: "row", justifyContent: "space-between", alignItems: "center", backgroundColor: "#fff", paddingHorizontal: 20, paddingVertical: 20, marginVertical: 15 },
+  review: { fontSize: 16, color: "#231F20" },
+  edit: { color: "#12D790", fontSize: 16 },
+  listContainer: { flex: 1, backgroundColor: "#FFF", marginTop: 30, paddingHorizontal: 10, paddingBottom: 25 },
+  title: { fontSize: 24, marginLeft: 20, marginTop: 15, marginBottom: 20, fontWeight: "bold" },
+  descContainer: { paddingHorizontal: 20 },
+  desc: { color: "#757575", marginBottom: 5 },
+  priceContainer: { flexDirection: "row", justifyContent: "space-between", alignItems: "center", paddingHorizontal: 20, marginVertical: 20 },
+  price: { fontSize: 24, fontWeight: "bold" }
 });
 
 const Button = (params) => {
   const btnStyle = {
-    backgroundColor: params.outline ? "#fff" : "#000",
+    backgroundColor: params.outline ? "#fff" : "#12D790",
     borderColor: params.outline ? "#000" : "#fff",
     borderWidth: 1
   };
@@ -279,13 +85,13 @@ const Button = (params) => {
 
 const buttonStyles = StyleSheet.create({
   btnContainer: {
-    paddingHorizontal: 5,
+    paddingHorizontal: 40,
     justifyContent: "center",
-    marginBottom: 20,
-    flex: 1
+    marginBottom: 10,
+    marginTop: 60
   },
   btn: {
-    backgroundColor: "black",
+    backgroundColor: "#12D790",
     height: 50,
     width: "100%",
     padding: 10,
@@ -307,3 +113,4 @@ const buttonStyles = StyleSheet.create({
     alignItems: "center"
   }
 });
+export default SubscriptionDetails;
