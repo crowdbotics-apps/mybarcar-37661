173ee51ffae99290fe0ab7a0c00fb74c017ec9ed
Crowdbotics <team@crowdbotics.com>
2023-01-31 01:53:51 +0000

    Add

--- screens/index.js
+++ screens/index.js
@@ -1,113 +1,108 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import {
   Text,
   StyleSheet,
   View,
-  SafeAreaView,
+  Image,
+  Pressable,
   TextInput,
-  Switch,
   ScrollView
 } from "react-native";
 
-const AccountSettingsScreen = (params) => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [cardNumber, setCardNumber] = useState("");
-  const [notifications, setNotifications] = useState(false);
-  const [emailNotifications, setEmailNotifications] = useState(false);
-  const [smsNotifications, setSmsNotifications] = useState(false);
-  const [deactivateAccount, setDeactivateAccount] = useState(false);
+const UserAddressScreen = (params) => {
+  const [order, setOrder] = useState({});
+  useEffect(() => {
+    setOrder({
+      orderName: "Order Name",
+      customerName: "Customer Name",
+      shopLocation: "S Main St, Los Angeles",
+      deliveryLocation: "Maple Ave , Los Angeles",
+      orderPrice: "$18",
+      shipping: "Free Shipping"
+    });
+  }, []);
+  const [address, setAddress] = useState("");
+  const [confirmAddress, setConfirmAddress] = useState("");
   return (
-    <SafeAreaView style={styles.container}>
+    <View style={styles.container}>
       <ScrollView>
-        <View style={styles.subContainer}>
-          <Text style={styles.subText}>Subscription</Text>
-          <View style={styles.subPallet}>
-            <View style={styles.planDes}>
-              <Text style={[styles.fnt25, styles.boldText]}>Plan</Text>
-              <Text style={styles.fnt16}>Description</Text>
+        <View style={styles.inputs}>
+          <Text style={styles.inputsHeading}>Delivery details</Text>
+          <View style={styles.inputContainer}>
+            <Text style={styles.inputText}>Address</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={(text) => setAddress(text)}
+              value={address}
+              placeholder="Maple Ave , Los Angeles"
+              placeholderTextColor="#9B9B9B"
+              autoCapitalize="none"
+              autoCorrect={false}
+            />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.inputText}>Verify Address</Text>
+            <TextInput
+              style={styles.input}
+              onChangeText={(text) => setConfirmAddress(text)}
+              value={confirmAddress}
+              placeholder="Enter"
+              placeholderTextColor="#9B9B9B"
+              autoCapitalize="none"
+              autoCorrect={false}
+            />
+            <Image
+              source={require("./assets/checkbox.png")}
+              style={styles.checkboxIcon}
+            />
+          </View>
+        </View>
+        <View style={styles.orderContainer}>
+          <Text style={styles.bold}>Order details</Text>
+          <View style={styles.shippingDetails}>
+            <Text style={styles.grey}>{order.shipping}</Text>
+            <Image source={require("./assets/Score.png")} />
+          </View>
+          <View style={styles.detailPallet}>
+            <View style={styles.orderDetails}>
+              <Text style={styles.mainText}>{order.orderName}</Text>
+              <Text style={styles.subText}>Additional info</Text>
             </View>
-            <View style={styles.subPricing}>
-              <Text style={[styles.fnt25, styles.boldText]}>$14.99 </Text>
-              <Text style={styles.fnt16}>per month</Text>
+            <View style={styles.pricing}>
+              <Text style={styles.mainText}>{order.orderPrice}</Text>
+              <Text style={styles.subText}>/Kg</Text>
+            </View>
+          </View>
+          <View style={styles.detailPallet}>
+            <View style={styles.orderDetails}>
+              <Text style={styles.mainText}>{order.customerName}</Text>
+              <Text style={styles.subText}>Additional info</Text>
+            </View>
+          </View>
+          <View style={styles.locationContainer}>
+            <Image
+              source={require("./assets/Progress.png")}
+              style={styles.progressImage}
+            />
+            <View style={styles.locationDetails}>
+              <View style={styles.shopLocation}>
+                <Text style={[styles.black, styles.fnt16]}>
+                  {order.shopLocation}
+                </Text>
+                <Text style={styles.grey}>Shop Location</Text>
+              </View>
+              <View style={styles.deliveryLocation}>
+                <Text style={[styles.black, styles.fnt16]}>
+                  {order.deliveryLocation}
+                </Text>
+                <Text style={styles.grey}>Delivery Location</Text>
+              </View>
             </View>
           </View>
         </View>
-        <View style={styles.billingContainer}>
-          <Text style={styles.billingText}>Billing information</Text>
-          <View style={styles.nameInput}>
-            <Text style={styles.inputText}>Full Name</Text>
-            <TextInput
-              style={styles.input}
-              placeholder="Enter your full name"
-              placeholderTextColor="#9B9B9B"
-              autoCapitalize="none"
-              autoCorrect={false}
-              value={name}
-              onChangeText={(text) => setName(text)}
-            />
-          </View>
-          <View style={styles.nameInput}>
-            <Text style={styles.inputText}>Email Address</Text>
-            <TextInput
-              style={styles.input}
-              placeholder="Enter your Email Address"
-              placeholderTextColor="#9B9B9B"
-              autoCapitalize="none"
-              autoCorrect={false}
-              value={email}
-              onChangeText={(text) => setEmail(text)}
-            />
-          </View>
-          <View style={styles.nameInput}>
-            <Text style={styles.inputText}>Card number</Text>
-            <TextInput
-              style={styles.input}
-              placeholder="Enter your Card Number"
-              placeholderTextColor="#9B9B9B"
-              autoCapitalize="none"
-              autoCorrect={false}
-              value={cardNumber}
-              onChangeText={(text) => setCardNumber(text)}
-            />
-          </View>
-        </View>
-        <View style={styles.togglesContainer}>
-          <View style={styles.toggle}>
-            <Text style={styles.toggleText}>Notifications</Text>
-            <Switch
-              style={styles.toggleSwitch}
-              value={notifications}
-              onValueChange={(value) => setNotifications(value)}
-            />
-          </View>
-          <View style={styles.toggle}>
-            <Text style={styles.toggleText}>Email Notifications</Text>
-            <Switch
-              style={styles.toggleSwitch}
-              value={emailNotifications}
-              onValueChange={(value) => setEmailNotifications(value)}
-            />
-          </View>
-          <View style={styles.toggle}>
-            <Text style={styles.toggleText}>SMS Notifications</Text>
-            <Switch
-              style={styles.toggleSwitch}
-              value={smsNotifications}
-              onValueChange={(value) => setSmsNotifications(value)}
-            />
-          </View>
-          <View style={styles.toggle}>
-            <Text style={styles.toggleText}>Deactivate Account</Text>
-            <Switch
-              style={styles.toggleSwitch}
-              value={deactivateAccount}
-              onValueChange={(value) => setDeactivateAccount(value)}
-            />
-          </View>
-        </View>
+        <Button buttonText={"Purchase"} />
       </ScrollView>
-    </SafeAreaView>
+    </View>
   );
 };
 
@@ -116,69 +111,93 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#fff"
   },
-  subContainer: {
-    paddingHorizontal: 20,
-    flex: 0.2,
-    justifyContent: "center"
-  },
-  subText: {
-    fontSize: 16,
-    fontWeight: "bold",
-    padding: 2,
-    marginVertical: 12,
-    marginLeft: 20
-  },
-  subPallet: {
+  orderContainer: {
     backgroundColor: "#fff",
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: "#e6e6e6",
-    padding: 10,
+    borderBottomRightRadius: 20,
+    borderBottomLeftRadius: 20,
+    paddingHorizontal: 20
+  },
+  shippingDetails: {
     flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center"
+    paddingVertical: 10,
+    alignItems: "center",
+    justifyContent: "space-between"
   },
-  planDes: {
-    flex: 0.4,
-    padding: 10,
-    // borderWidth: 1,
-    // borderColor: '#979797',
-    justifyContent: "center",
-    alignItems: "flex-start"
-  },
-  subPricing: {
-    flex: 0.6,
-    padding: 10,
-    // borderWidth: 1,
-    // borderColor: '#979797',
-    flexDirection: "row",
-    alignItems: "flex-end",
-    justifyContent: "center"
-  },
-  boldText: {
+  bold: {
     fontWeight: "bold"
   },
-  fnt25: {
-    fontSize: 25
+  grey: {
+    color: "#8e8e8e"
+  },
+  black: {
+    color: "#000"
   },
   fnt16: {
     fontSize: 16
   },
-  billingContainer: {
-    flex: 0.5,
-    paddingHorizontal: 20 // borderWidth: 1,
-    // borderColor: '#979797',
+  detailPallet: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 10,
+    alignItems: "center",
+    borderBottomWidth: 1,
+    borderBottomColor: "#e6e6e6"
   },
-  billingText: {
-    fontSize: 16,
+  orderDetails: {
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "space-between",
+    paddingRight: 10
+  },
+  pricing: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "flex-end"
+  },
+  mainText: {
+    fontSize: 20
+  },
+  subText: {
+    fontSize: 14,
+    color: "#8e8e8e",
+    marginTop: 5
+  },
+  locationContainer: {
+    flexDirection: "row",
+    paddingTop: 20
+  },
+  locationDetails: {
+    flex: 1,
+    flexDirection: "column",
+    marginLeft: 10,
+    justifyContent: "space-between",
+    alignItems: "flex-start"
+  },
+  inputs: {
+    paddingHorizontal: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: "#e6e6e6",
+    paddingBottom: 20,
+    marginBottom: 20,
+    marginHorizontal: 5
+  },
+  inputsHeading: {
+    fontSize: 14,
     fontWeight: "bold",
-    padding: 2,
-    marginVertical: 12,
-    marginLeft: 20
+    marginBottom: 10,
+    color: "#000",
+    marginLeft: 10
+  },
+  inputContainer: {
+    flexDirection: "column",
+    justifyContent: "center",
+    marginHorizontal: 5
   },
   inputText: {
-    fontSize: 16,
-    marginLeft: 20
+    fontSize: 14,
+    marginLeft: 20,
+    color: "#111112"
   },
   input: {
     borderWidth: 1,
@@ -187,23 +206,62 @@ const styles = StyleSheet.create({
     padding: 10,
     paddingLeft: 20,
     marginVertical: 10,
-    width: "100%"
+    width: "100%",
+    height: 50
   },
-  togglesContainer: {
-    flex: 0.3,
-    paddingHorizontal: 20
-  },
-  toggle: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center"
-  },
-  toggleText: {
-    fontSize: 16,
-    fontWeight: "bold",
-    padding: 2,
-    marginVertical: 12,
-    marginLeft: 20
+  checkboxIcon: {
+    position: "absolute",
+    right: 30,
+    top: 45
   }
 });
-export default AccountSettingsScreen;
+export default UserAddressScreen;
+const Button = (params) => {
+  const btnStyle = {
+    backgroundColor: params.outline ? "#fff" : "#000",
+    borderColor: params.outline ? "#000" : "#fff",
+    borderWidth: 1
+  };
+  const btnText = {
+    color: params.outline ? "#000" : "#fff"
+  };
+  return (
+    <View style={buttonStyles.btnContainer}>
+      <Pressable style={[buttonStyles.btn, btnStyle]} onPress={params.onPress}>
+        <Text style={[buttonStyles.btnText, btnText]}>{params.buttonText}</Text>
+        <View style={styles.childrenContainer}>{params.children}</View>
+      </Pressable>
+    </View>
+  );
+};
+
+const buttonStyles = StyleSheet.create({
+  btnContainer: {
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    justifyContent: "center",
+    marginTop: 20
+  },
+  btn: {
+    backgroundColor: "black",
+    height: 50,
+    width: "100%",
+    padding: 10,
+    paddingHorizontal: 25,
+    borderRadius: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    shadowColor: "rgba(0, 0, 0, 0.2)",
+    elevation: 10,
+    flexDirection: "row"
+  },
+  btnText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold"
+  },
+  childrenContainer: {
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
